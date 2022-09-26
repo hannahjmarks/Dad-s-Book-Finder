587e9ec872010d436c3a30b4ae40dc68e6a8f99c
hannahjmarks <hannah.j.marks@live.co.uk>
2022-09-26 21:08:28 +0100

    Moved Class Library into solution

--- Dads Book Finder/Dads Book Finder/obj/Debug/net6.0-windows/App.g.i.cs
+++ Dads Book Finder/Dads Book Finder/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Dads_Book_Finder {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Dads Book Finder;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dads Book Finder;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
